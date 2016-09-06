53db3e69bf91ca1920b6a3c0eb938be44bcee94a
Pablongas <pablo.moyano89@gmail.com>
2016-09-06 11:25:58 -0300

    ! agregados clocking

---
+++ Assets/Scripts/Clocking.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clocking : MonoBehaviour {
+
+    public Transform hoursTransform;
+    public Transform minutesTransform;
+    public Transform secondsTransform;
+
+
+
+
+}
